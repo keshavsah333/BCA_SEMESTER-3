0f30bbaa1093bb84d35ca11209088672aa364242
keshavsah333 <keshavsah333@gmail.com>
2026-08-03 19:11:55 +0530

    update

--- python/bca_sem-3.py
+++ python/bca_sem-3.py
@@ -1,2 +1,13 @@
+if 2%2==0:
+    print("The number is even.")
+else:
+    print("the number is odd.")
+    
 
+# github copilot even and odd statement
+
+if n % 2 == 0:
+    print(f"{n} is even")
+else:
+    print(f"{n} is odd")
 
