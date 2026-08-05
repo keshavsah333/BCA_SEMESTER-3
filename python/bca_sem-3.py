4739b08e4b0a451b5e74d9e262b65a855a712855
keshavsah333 <keshavsah333@gmail.com>
2026-08-05 18:47:48 +0530

    update

--- python/bca_sem-3.py
+++ python/bca_sem-3.py
@@ -13,3 +13,10 @@ else:
 
 
 #know i have to check if the number is prime or not
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
