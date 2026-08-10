6de6f27455e9fdb8a2c2d3a37b20954907d89c47
keshavsah333 <keshavsah333@gmail.com>
2026-08-10 17:43:41 +0530

    update

--- assignment-2.py
+++ assignment-2.py
@@ -12,7 +12,7 @@ print("Bitwise NOT (~) of first number =", ~a)
 print("Left Shift (<<) =", a << 1)
 print("Right Shift (>>) =", a >> 1)
 
-'''
+
 # Q2. Grade a student based on marks
 marks = int(input("Enter marks: "))
 
@@ -28,3 +28,41 @@ else:
     grade = "F"
 
 print("Grade =", grade)
+
+
+#Q3. Use nested loops to print the given pattern
+for i in range(1, 6):
+    for j in range(1, i + 1):
+        print(j, end="")
+    print()
+
+#Q4. Display day using match-case
+day = int(input("Enter a number (1-7): "))
+
+match day:
+    case 1:
+        print("Monday")
+    case 2:
+        print("Tuesday")
+    case 3:
+        print("Wednesday")
+    case 4:
+        print("Thursday")
+    case 5:
+        print("Friday")
+    case 6:
+        print("Saturday")
+    case 7:
+        print("Sunday")
+    case _:
+        print("Invalid input")    
+'''
+#Q5. Perform operations on a string
+text = input("Enter a string: ")
+
+print("Number of characters =", len(text))
+print("Uppercase =", text.upper())
+print("Lowercase =", text.lower())
+print("First character =", text[0])
+print("Last character =", text[-1])
+print("Without whitespaces =", text.replace(" ", ""))
