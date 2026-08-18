d7a0d4dff3153c9dfaef746bbca5ad2977971a20
keshavsah333 <keshavsah333@gmail.com>
2026-08-18 18:30:54 +0530

    id

--- python/bca_sem-3.py
+++ python/bca_sem-3.py
@@ -55,3 +55,8 @@ i in range(2, int(n**0.5) + 1):
 def is_prime(n):
     if n <= 1:
 
+i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+#know i have to check if the numb
