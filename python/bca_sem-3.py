8354f747f941d80b87c3f414e4bd36df69297892
keshavsah333 <keshavsah333@gmail.com>
2026-08-26 08:35:30 +0530

    update

--- python/bca_sem-3.py
+++ python/bca_sem-3.py
@@ -135,29 +135,7 @@ i in range(2, int(n**0.5) + 1):
 
 , int(n**0.5) + 1):
         if n % i == 0:
-            return False
-    return True
-
-, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-
-, 
-
-int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-
-int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return Trueint(n**0.5) + 1):
+            retur
         if n % i == 0:
             return False
     return True
