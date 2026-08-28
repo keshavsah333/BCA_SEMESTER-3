60f958f71b12087c39b0bb51e56a69fd20d0cdb9
keshavsah333 <keshavsah333@gmail.com>
2026-08-28 22:19:11 +0530

    update

--- python/bca_sem-3.py
+++ python/bca_sem-3.py
@@ -233,3 +233,11 @@ if 2%4==0:
         else:
         print("the number is odd.")
 
+    if 2 % 2 == 0:
+        print("The number is even.")
+else:
+    print("the number is odd.")
+
+    else:
+    print("the number is odd.")
+
