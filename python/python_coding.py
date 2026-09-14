3d9fb1b58009a87dad3d89c979a963281dc91a62
keshavsah333 <keshavsah333@gmail.com>
2026-09-14 22:05:45 +0530

    up

---
+++ python/python_coding.py
@@ -0,0 +1,4 @@
+# list - list is a collection items that you can change (add,remove, edit)
+fruits = ["apple", "banana", "mango", "orange"]
+
+# tuple - tuple is a collection of items that cannot be changed (immutable)
