f2a953bd895a5b9f415817d2dc1742d240852049
keshavsah333 <keshavsah333@gmail.com>
2026-09-14 22:16:03 +0530

    up

--- python/python_Assignment 8-Functions 2.py
+++ python/python_Assignment 8-Functions 2.py
@@ -1,1 +1,7 @@
 #1. Given a list of tuples representing student scores: students = [("Alice", 88), ("Bob", 92), ("Charlie", 78)].  Create a function to sort this list by the students' scores in descending order. 
+# Python code to test that 
+# tuples are immutable 
+  
+tuple1 = (0, 1, 2, 3) 
+tuple1[0] = 4
+print(tuple1)
