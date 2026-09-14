3d451e565525fce9d8b3f54ca9acee753057ad70
keshavsah333 <keshavsah333@gmail.com>
2026-09-14 21:44:31 +0530

    up

--- python/python_Assignment 8-Functions 2.py
+++ python/python_Assignment 8-Functions 2.py
@@ -1,2 +1,5 @@
+#1. Given a list of tuples representing student scores: students = [("Alice", 88), ("Bob", 92), ("Charlie", 78)].  Create a function to sort this list by the students' scores in descending order. 
+# list - list is a collection items that you can change (add,remove, edit)
+fruits = ["apple", "banana", "mango", "orange"]
 
-
+# tuple - tuple is a collection of items that cannot be changed (immutable)
