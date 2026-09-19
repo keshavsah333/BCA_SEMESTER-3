a4e299469e7b77200db55d11be573bd3e06adff9
keshavsah333 <keshavsah333@gmail.com>
2026-09-19 16:42:45 +0530

    update

--- python/python_coding.py
+++ python/python_coding.py
@@ -1,4 +1,15 @@
 # list - list is a collection items that you can change (add,remove, edit)
-fruits = ["apple", "banana", "mango", "orange"]
+#fruits = ["apple", "banana", "mango", "orange"]
 
 # tuple - tuple is a collection of items that cannot be changed (immutable)
+
+#s = "PYTHON"
+
+#print(s[::-1])
+
+# [::-1] for reversing the string in 'STRING SLICING'
+
+'''s = "python or"
+
+print(s.upper())
+'''
